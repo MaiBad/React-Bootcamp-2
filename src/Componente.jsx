2de8b3edd1b38f70696a4a3d0bae1f39358f4858
MaiBad <c.jeferson.lc@gmail.com>
2022-07-22 23:50:35 -0500

    func-component

---
+++ src/Componente.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+
+const Componente = () => {
+
+    const initialState = {
+        fecha: new Date(),
+        edad: 0,
+        nombre: 'Jeferson',
+        apellidos: 'Limay',
+    }
+
+    const [state, setState] = useState(initialState)
+
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            tick()
+        }, 1000);
+        return () => {
+            clearInterval(timer)
+        }
+    })
+
+    const tick = () => {
+        return setState({
+            fecha: new Date(),
+            edad: state.edad + 1,
+            nombre: state.nombre,
+            apellidos: state.apellidos,
+        })
+    }
+    return (
+        <div>
+            <h2>
+                Hora Actual: 
+                {state.fecha.toLocaleTimeString()}
+            </h2>
+            <h3>{state.nombre} {state.apellidos}</h3>
+            <h1>Edad: {state.edad}</h1>
+        </div>
+    )
+
+}
+
+export default Componente
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Componente from './Componente';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Componente></Componente>
+  </React.StrictMode>
+);
